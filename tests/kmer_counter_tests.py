a6305318d0d31c3c65e6244013f4faf97eb6398f
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-01-13 11:26:43 +0100

    Refactor: extract utility function for checking whether features are sorted

--- tests/kmer_counter_tests.py
+++ tests/kmer_counter_tests.py
@@ -3,6 +3,10 @@ import unittest
 from kmer_counter import KmerCounter
 
 
+def is_kmer_list_sorted(res):
+    return all(res[i][0] < res[i + 1][0] for i in range(len(res) - 1))
+
+
 class MyTestCase(unittest.TestCase):
 
     def test_seq_of_one_kmer_gives_count_1_only_for_one_entry(self):
@@ -34,7 +38,7 @@ class MyTestCase(unittest.TestCase):
         kmer = "ACTAA"
         res = KmerCounter(kmer).get_kmer_frequencies_sorted_list(k)
         self.assertEqual(len(res), 136)
-        self.assertTrue(all(res[i][0] < res[i + 1][0] for i in range(len(res) - 1)))  # check if sorted
+        self.assertTrue(is_kmer_list_sorted(res))  # check if sorted
 
 
 if __name__ == '__main__':
