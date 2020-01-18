7617694634778bce802efb2c6dff56fbe85071cf
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-01-19 00:05:03 +0100

    kmer_space_tests: refactor: rename a test method

--- tests/kmer_space_tests.py
+++ tests/kmer_space_tests.py
@@ -12,7 +12,7 @@ class KMerSpaceTest(unittest.TestCase):
         kmer_space = KMerSpace(k=4, alphabet=common_alphabet)
         self.assertEqual(len(kmer_space.get_sorted_by_kmer()), 136)
 
-    def test_increase_1_a_key_that_is_explicitly_in_dict(self):
+    def test_adding_kmer(self):
         kmer_space = KMerSpace(k=4, alphabet=common_alphabet)
         kmer = "ACTG"
         kmer_space.increase(kmer)
