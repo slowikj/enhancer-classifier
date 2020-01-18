835793da5cfac8f555b1ea61ca53158ef3785ba8
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-01-19 00:18:05 +0100

    KMerCounter: add get_frequencies

--- kmer_counter.py
+++ kmer_counter.py
@@ -8,6 +8,10 @@ class KMerCounter:
     def __init__(self, kmer_space: KMerSpace):
         self.kmer_space = kmer_space
 
+    def get_frequencies(self, seq):
+        counts = self.count(seq)
+        return map(lambda x: (x[0], x[1] / len(seq)), counts)
+
     def count(self, seq):
         seq = seq.upper()
 
