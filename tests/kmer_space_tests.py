ef49b48d66fc277cd495e676c0552adb5dff4396
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-01-19 00:02:52 +0100

    kmer_space_tests: add test checking sorting order

--- tests/kmer_space_tests.py
+++ tests/kmer_space_tests.py
@@ -1,6 +1,7 @@
 import unittest
 
 from kmer_space import KMerSpace
+from tests.utils import is_kmer_list_sorted
 
 common_alphabet = ["A", "C", "T", "G"]
 
@@ -16,4 +17,9 @@ class KMerSpaceTest(unittest.TestCase):
         kmer = "ACTG"
         kmer_space.increase(kmer)
         self.assertEqual(kmer_space.get_value(kmer), 1)
+
+    def test_is_output_kmer_list_is_sorted_by_kmer(self):
+        kmer_space = KMerSpace(k=4, alphabet=common_alphabet)
+        res = kmer_space.get_sorted_by_kmer()
+        self.assertTrue(is_kmer_list_sorted(res))
 
