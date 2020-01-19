1ada8cfe73adc2b044a64544ec19dac878974b05
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-01-19 17:19:23 +0100

    Save result probabilities

--- main.py
+++ main.py
@@ -12,20 +12,30 @@ frame_length = 1500
 step = 750
 
 
-def get_probabilities_for_1_list(valid_frames, invalid_frames, y_test_proba):
+def get_probabilities_for_1_list(valid_frames, invalid_frames, y_test_valid_proba):
     prob_for_1 = [0] * (len(valid_frames) + len(invalid_frames))
-    print(len(prob_for_1))
+    
     for valid_frame_ind in range(len(valid_frames)):
         prob_for_1_ind = int(valid_frames[valid_frame_ind].begin / step)
-        prob_for_1[prob_for_1_ind] = y_test_proba[valid_frame_ind][1]
+        prob_for_1[prob_for_1_ind] = y_test_valid_proba[valid_frame_ind][1]
 
-    mean_prob = np.mean(y_test_proba[:, 1], axis=0)
+    mean_prob = np.mean(y_test_valid_proba[:, 1], axis=0)
 
     for invalid_frame_ind in range(len(invalid_frames)):
         prob_for_1_ind: int = int(invalid_frames[invalid_frame_ind].begin / step)
         prob_for_1[prob_for_1_ind] = mean_prob
 
     return np.array(prob_for_1)
+
+
+def save_as_wig(filename, probabilities):
+    new_shape = (len(probabilities), 1)
+    indices = np.arange(new_shape[0]) * step + 1
+    indices.dtype = "int64"
+    indices = indices.reshape(new_shape)
+    probabilities = probabilities.reshape(new_shape)
+    data = np.concatenate((indices, probabilities), axis=1)
+    np.savetxt(filename, data, fmt=["%d", "%.2f"])
 
 
 if __name__ == "__main__":
@@ -38,11 +48,6 @@ if __name__ == "__main__":
     X_test = create_features(valid_frames,
                              alphabet=alphabet,
                              k=k)
-    y_test_proba = clf.predict_proba(X_test)
-    print(y_test_proba)
-
-    print(len(valid_frames))
-    print(len(invalid_frames))
-    print("------------")
-    res = get_probabilities_for_1_list(valid_frames, invalid_frames, y_test_proba)
-    print(res)
+    y_test_valid_proba = clf.predict_proba(X_test)
+    y_test_all_proba = get_probabilities_for_1_list(valid_frames, invalid_frames, y_test_valid_proba)
+    save_as_wig("./output/chr_prob.wig", y_test_all_proba)
