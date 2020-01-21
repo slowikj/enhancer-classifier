1c8d5e7562f2f2c35dcfbaa062c9a1d7d94fa9c8
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-01-21 15:03:14 +0100

    Change metric: f1 -> auc

--- train_model.py
+++ train_model.py
@@ -1,6 +1,6 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import VarianceThreshold, chi2, SelectKBest
-from sklearn.metrics import make_scorer, f1_score
+from sklearn.metrics import make_scorer, f1_score, roc_auc_score
 from sklearn.model_selection import StratifiedKFold, cross_val_score, GridSearchCV
 from sklearn.pipeline import Pipeline
 
@@ -19,7 +19,7 @@ def train_model(X, y):
         ("kbest", SelectKBest(chi2)),
         ("randomForest", random_forest)
     ])
-    rs = GridSearchCV(pipeline, hyperparams, verbose=1, cv=inner_cv, scoring=make_scorer(f1_score), n_jobs=8)
+    rs = GridSearchCV(pipeline, hyperparams, verbose=1, cv=inner_cv, scoring=make_scorer(roc_auc_score), n_jobs=8)
     rs.fit(X, y)
     scores = cross_val_score(rs, X, y, cv=outer_cv)
     print(rs.best_score_)
